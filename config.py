043200428bef067aacdb71a6baa3a1b7168178f5
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-11 17:59:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(environ.get("API_ID", 27190467))
     API_HASH = environ.get("API_HASH", "ff6bc6ad2faba520f426cf04ca7f5773")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7240304290:AAGfIJUgfukO1ALuhnhZS21g980zpL57VQM")
-    AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "6066102279 -1002275998071 5574593875").split(" "))
+    AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "-1002275998071").split(" "))
     OWNER_ID = int(environ.get("OWNER_ID", 6066102279))
     CREDITS = environ.get("CREDITS", "SharkToonsIndia")  # Default value is "SharkToonsIndia"
     DOWNLOAD_DIRECTORY = environ.get("DOWNLOAD_DIRECTORY", "./downloads")
