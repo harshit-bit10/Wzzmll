8c895d1948c9c6f7e88ffa4c5e3288f1778d42ca
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-15 14:22:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config(object):
     API_ID = int(environ.get("API_ID", 27190467))
     API_HASH = environ.get("API_HASH", "ff6bc6ad2faba520f426cf04ca7f5773")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7240304290:AAGfIJUgfukO1ALuhnhZS21g980zpL57VQM")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7240304290:AAFkHLSw_rSnIZRG0TVCcR2t87l_TcHzfeI")
     
     # Combine AUTH_USERS and AUTH_GROUPS into a single list
     AUTH_USERS = list(int(x) for x in environ.get("AUTH_USERS", "6066102279 1885207148 6623741903").split(" "))
